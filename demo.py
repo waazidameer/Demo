21f11c51081eb8434676f2d7f93519f17261c31a
waazidameer <124231924+waazidameer@users.noreply.github.com>
2023-12-29 12:11:33 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,11 +1,13 @@
 import streamlit as st
 st.set_page_config(page_title='cats')
 st.markdown("## Types Of Cats")
-st.image("./persian.jpg.jpg")
-
-st.write("persian cat")
-st.image("./ragdoll.jpg")
-
-st.write("ragdoll cat")
-st.image("https://img.freepik.com/free-photo/beautiful-domestic-cat-laying-fence_181624-43207.jpg?w=996&t=st=1703830370~exp=1703830970~hmac=86d0c9640678053e1beb6c04aaa300853bfd967deea3433fd48f5dd0bbbaecfd")
-
+col1,col2=st.columns(2)
+with col1:
+  st.subheader("persian Cat")
+  st.image("./persian.jpg.jpg,width=300,use_column_width=True")
+  st.write("persian cats are Cute")
+with col2:
+  st.subheader("ragdoll Cat")
+  st.image("./ragdoll.jpg,width=300,use_column_width=True")
+  st.write("ragdoll cats are Cute")
+  
