ffaecf1ad73bd64e787dc45c336ce9fa6f5e13c5
waazidameer <124231924+waazidameer@users.noreply.github.com>
2023-12-29 12:13:40 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -4,10 +4,10 @@ st.markdown("## Types Of Cats")
 col1,col2=st.columns(2)
 with col1:
   st.subheader("persian Cat")
-  st.image("./persian.jpg.jpg,width=300,use_column_width=True")
+  st.image("./persian.jpg.jpg",width=300,caption="persian cat",use_column_width=True)
   st.write("persian cats are Cute")
 with col2:
   st.subheader("ragdoll Cat")
-  st.image("./ragdoll.jpg,width=300,use_column_width=True")
+  st.image("./ragdoll.jpg",width=300,caption="persian cat",use_column_width=True)
   st.write("ragdoll cats are Cute")
   
