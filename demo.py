bd85a3d65154efd2726fd9223f16dd01d55958e5
waazidameer <124231924+waazidameer@users.noreply.github.com>
2023-12-29 11:53:43 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -4,7 +4,7 @@ st.markdown("## Types Of Cats")
 st.image("./persian.jpg.jpg")
 
 st.write("persian cat")
-st.image("./ragdoll.jpg.jpg")
+st.image("./ragdoll.jpg")
 
 st.write("ragdoll cat")
 st.image("https://img.freepik.com/free-photo/beautiful-domestic-cat-laying-fence_181624-43207.jpg?w=996&t=st=1703830370~exp=1703830970~hmac=86d0c9640678053e1beb6c04aaa300853bfd967deea3433fd48f5dd0bbbaecfd")
